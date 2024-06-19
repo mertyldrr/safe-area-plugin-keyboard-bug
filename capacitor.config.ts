8ce5e5d9fa88a8d7fa0e78bf7a235f2395b78691
mertyldrr <mertyildirir14@gmail.com>
2024-06-19 15:56:10 +0200

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'safe-area-plugin-keyboard',
+  webDir: 'www'
+};
+
+export default config;
