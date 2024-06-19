cd96a07a1a2a65a92f1874236812421c23a69320
mertyldrr <mertyildirir14@gmail.com>
2024-06-19 16:22:51 +0200

    create minimal app for reproduction

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -3,7 +3,18 @@ import type { CapacitorConfig } from '@capacitor/cli';
 const config: CapacitorConfig = {
   appId: 'io.ionic.starter',
   appName: 'safe-area-plugin-keyboard',
-  webDir: 'www'
+  webDir: 'www',
+  "plugins": {
+    "SafeArea": {
+      "enabled": true,
+      "customColorsForSystemBars": true,
+      "statusBarColor": '#e23000',
+      "statusBarContent": 'light',
+      "navigationBarColor": '#e23000',
+      "navigationBarContent": 'light',
+      "offset": 0
+    }
+  }
 };
 
 export default config;
